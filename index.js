6047f4cda2e46e69f225be4ee64c8cd5140dbbd4
Kritica Rani <120586121+Kritica-rani@users.noreply.github.com>
2024-05-12 12:59:07 +0530

    added some files

---
+++ index.js
@@ -0,0 +1,24 @@
+// function test() {
+//   console.log("a", a); //ERROR
+//   let a = 30;
+
+//   return a + a;
+// }
+// test();
+
+//Scope
+//global scoped/functional
+const api = "https://jsonplaceholder.typicode.com/posts";
+const fetchData = async () => {
+  try {
+    const data = await fetch(api);
+    const result = await data.json();
+    renderDataOnUi(result);
+  } catch (err) {
+    console.log("error in fetching data", err);
+  }
+};
+const renderDataOnUi = (data) => {
+  console.log("data", data);
+};
+fetchData();
